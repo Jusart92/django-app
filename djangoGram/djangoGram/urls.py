ed7c0b98586c21ab04a78033137a52c1d698464c
Arturo <arturo.juarez@globaloxs.com>
2020-12-20 09:13:17 -0600

    make mirgations, nueva tabla post_users

--- djangoGram/djangoGram/urls.py
+++ djangoGram/djangoGram/urls.py
@@ -1,9 +1,12 @@
 """ URLs modeule"""
 from django.urls import path
-from djangoGram import views
+from djangoGram import views as local_views
+from posts import views as posts_views
 
 urlpatterns = [
-    path('hello-world/', views.hello_world),
-    path('sorted/', views.sorted_numbers),
-    path('hi/<str:name>/<int:age>', views.say_hi),
+    path('hello-world/', local_views.hello_world),
+    path('sorted/', local_views.sorted_numbers),
+    path('hi/<str:name>/<int:age>', local_views.say_hi),
+
+    path('posts/', posts_views.list_posts),
 ]
