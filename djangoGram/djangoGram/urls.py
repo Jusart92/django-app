90c0f7185da7642fa955696eb6024bf72a66fa60
Arturo <arturo.juarez@globaloxs.com>
2020-12-20 22:06:18 -0600

    dashboard de administracion

--- djangoGram/djangoGram/urls.py
+++ djangoGram/djangoGram/urls.py
@@ -1,9 +1,13 @@
 """ URLs modeule"""
+from django.contrib import admin
 from django.urls import path
 from djangoGram import views as local_views
 from posts import views as posts_views
 
 urlpatterns = [
+
+    path('admin/', admin.site.urls),
+
     path('hello-world/', local_views.hello_world),
     path('sorted/', local_views.sorted_numbers),
     path('hi/<str:name>/<int:age>', local_views.say_hi),
