cdec2df5b195611ecc0b68d921984b5bcfb885c1
Arturo <arturo.juarez@globaloxs.com>
2020-12-21 23:47:56 -0600

    login logout

--- djangoGram/posts/views.py
+++ djangoGram/posts/views.py
@@ -2,6 +2,7 @@
 
 # Django
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from datetime import datetime
 
@@ -36,7 +37,7 @@ posts = [
 ]
 
 
-# Create your views here.
+@login_required
 def list_posts(request):
     """list existing posts"""
     return render(request, 'posts/feed.html', {'posts': posts})
