d0a5514e99ff2145f7c238a6060c4c81b9ba8de5
Arturo <arturo.juarez@globaloxs.com>
2020-12-29 09:17:12 -0600

    bootstrap a sign up

--- djangoGram/users/forms.py
+++ djangoGram/users/forms.py
@@ -5,26 +5,79 @@ from django import forms
 
 # Models
 from django.contrib.auth.models import User
+from users.models import Profile
 
 
 class SingupForm(forms.Form):
     """Signup form."""
-    username = forms.CharField(min_length=4, max_length=50)
-
-    password = forms.CharField(max_length=70, widget=forms.PasswordInput())
-    password_confirmation = forms.CharField(
-        max_length=70,
-        widget=forms.PasswordInput()
+    username = forms.CharField(
+        min_length=4,
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Username',
+                'class': 'form-control',
+                'required': True
+                }
         )
+    )
+    password = forms.CharField(
+        min_length=6,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Password',
+                'class': 'form-control',
+                'required': True
+            }
+        )
+    )
+    password_confirmation = forms.CharField(
+        min_length=6,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Password Confirmation',
+                'class': 'form-control',
+                'required': True
+            }
+        )
+    )
 
-    first_name = forms.CharField(min_length=2, max_length=50)
-    last_name = forms.CharField(min_length=2, max_length=50)
+    first_name = forms.CharField(
+        min_length=3,
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'First name',
+                'class': 'form-control',
+                'required': True
+                }
+            )
+        )
+    last_name = forms.CharField(
+        min_length=3,
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Last name',
+                'class': 'form-control',
+                'required': True
+                }
+        )
+    )
 
     email = forms.CharField(
         min_length=6,
         max_length=70,
-        widget=forms.EmailInput()
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "email",
+                "class": "form-control",
+                'required': True
+            }
         )
+    )
 
     def clean_username(self):
         """Username must be unique"""
@@ -44,6 +97,16 @@ class SingupForm(forms.Form):
             raise forms.ValidationError('Password do no match')
 
         return data
+
+    def save(self):
+        """Create user and profile"""
+        data = self.cleaned_data
+        data.pop('password_confirmation')
+
+        user = User.objects.create_user(**data)
+
+        profile = Profile(user=user)
+        profile.save()
 
 
 class ProfileForm(forms.Form):
