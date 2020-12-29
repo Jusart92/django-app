046facc433aa789bbc2a5b300a86b885c774922b
Arturo <arturo.juarez@globaloxs.com>
2020-12-29 08:37:53 -0600

    uso de validaciones del formulario de django

--- djangoGram/users/forms.py
+++ djangoGram/users/forms.py
@@ -3,6 +3,48 @@
 # Django
 from django import forms
 
+# Models
+from django.contrib.auth.models import User
+
+
+class SingupForm(forms.Form):
+    """Signup form."""
+    username = forms.CharField(min_length=4, max_length=50)
+
+    password = forms.CharField(max_length=70, widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(
+        max_length=70,
+        widget=forms.PasswordInput()
+        )
+
+    first_name = forms.CharField(min_length=2, max_length=50)
+    last_name = forms.CharField(min_length=2, max_length=50)
+
+    email = forms.CharField(
+        min_length=6,
+        max_length=70,
+        widget=forms.EmailInput()
+        )
+
+    def clean_username(self):
+        """Username must be unique"""
+        username = self.cleaned_data['username']
+        username_taken = User.objects.filter(username=username).exists()
+        if username_taken:
+            raise forms.ValidationError('Username is already in user.')
+        return username
+
+    def clean(self):
+        """Verify password confirmation match"""
+        data = super().clean()
+
+        password = data['password']
+        password_confirmation = data['password_confirmation']
+        if password != password_confirmation:
+            raise forms.ValidationError('Password do no match')
+
+        return data
+
 
 class ProfileForm(forms.Form):
     """Profile form."""
