46162003b2339f6cc850535db4f9f7042cd50661
Arturo <arturo.juarez@globaloxs.com>
2020-12-31 09:33:48 -0600

    Detail para usuario, postview

---
+++ djangoGram/posts/urls.py
@@ -0,0 +1,20 @@
+"""Post URLs."""
+
+# Django
+from django.urls import path
+
+# Views
+from posts import views
+
+urlpatterns = [
+    path(
+        route='',
+        view=views.PostFeedView.as_view(),
+        name='feed'
+        ),
+    path(
+        route='posts/new/',
+        view=views.create_post,
+        name='create'
+        ),
+]
