1896776268f72b91185c14e3916b81e90a653806
Arturo Juarez <jusart.92@gmail.com>
2021-12-28 16:58:51 -0600

    forms

--- djangoGram/users/forms.py
+++ djangoGram/users/forms.py
@@ -7,7 +7,7 @@ from django import forms
 class ProfileForm(forms.Form):
     """Profile form."""
 
-    website = forms.URLField(max_length=200, required=True)
-    biography = forms.CharField(max_length=500, required=False)
+    website = forms.URLField(max_length=200, required=False)
+    biography = forms.CharField(max_length=500, required=True)
     phone_number = forms.CharField(max_length=20, required=False)
-    picture = forms.ImageField(required=False)
+    picture = forms.ImageField(required=True)
